e76a89974e4ad4183ce2832a5b0d2254c082e3ac
polinafedorenko <polina.fedorenko@kyburz-switzerland.ch>
2025-11-11 17:55:41 +0100

    Added CI configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/file-uploadify/',
     test: {
         environment: 'jsdom',
         setupFiles: ['./src/client/test-setup.ts'],
